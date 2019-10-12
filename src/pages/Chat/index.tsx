b561f9d2efa04843e0f74bb837332bcb1fda2044
권재원 <springkjw@gmail.com>
2019-10-12 15:02:33 +0900

    채팅방 임시

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Chat:React.FC = _ => {
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     const socket = new WebSocket('ws://localhost:3000/ws');
 
@@ -21,6 +23,14 @@ const Chat:React.FC = _ => {
   return (
     <div>
       <h1>채팅방</h1>
+      {
+        messages.map((message, idx) => {
+          return (
+            <div>{message}</div>
+          );
+        })
+      }
+      <textarea></textarea>
     </div>
   );
 };
