da36d6f788aee625bdd365074b96a820b5f81ddd
groovypark <soobin950@gmail.com>
2019-10-23 18:01:32 +0900

    아이콘 이미지 파일 업로드

--- src/components/Button/style.tsx
+++ src/components/Button/style.tsx
@@ -4,13 +4,13 @@ import { Style } from './interface';
 export const Button = styled.button`
   width: ${(props: Style) => `${props.width}px`};
   height: ${(props: Style) => `${props.height}px`};
-  border-radius: ${(props: Style) => `${props.borderRadius}px`};
-  font-size: ${(props: Style) => `${props.fontSize}px`};
+  border-radius: ${(props: Style) => props.borderRadius ? `${props.borderRadius}px` : '4px'};
+  font-size: ${(props: Style) => props.fontSize ? `${props.fontSize}px` : '16px'};
   font-weight: ${(props: Style) => props.bold ? 'bold' : 400};
   margin: ${(props: Style) => props.margin};
   border: 0;
-  cursor: pointer;
+  cursor: ${(props: Style) => props.cursor ? props.cursor : 'pointer'};
   box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.24);
-  background-color: #ffffff;
-  color: #000000;
+  color: ${(props: Style) => props.color};
+  background: ${(props: Style) => props.background ? props.background : 'white'};
 `;
