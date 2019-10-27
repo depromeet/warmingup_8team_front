0d81b9cf10f9cbe1d110daa483ac51431754dff2
권재원 <springkjw@gmail.com>
2019-10-27 17:27:24 +0900

    로그인을 해야하는데 안한 경우 강제 로그인 페이지 리다이렉션 프로텍트 추가

--- src/routes/Route/index.tsx
+++ src/routes/Route/index.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
-import { Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import { RootState } from 'store/reducers/interface';
 import { Props } from './interface';
 
 const CutomRoute:React.FC<Props> = ({
@@ -8,15 +10,27 @@ const CutomRoute:React.FC<Props> = ({
   exact = false,
   loginRequired = false,
 }) => {
+  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+  const [valid, setValid] = useState(true);
+  const [redirectPath, setRedirectPath] = useState('/');
+
   useEffect(() => {
+    if (loginRequired && !isLoggedIn) {
+      setRedirectPath('/login');
+      setValid(false);
+    }
   }, []);
 
   return (
-    <Route
-      exact={exact}
-      path={path}
-      component={component}
-    />
+    valid ? (
+      <Route
+        exact={exact}
+        path={path}
+        component={component}
+      />
+    ) : (
+      <Redirect to={redirectPath} />
+    )
   );
 };
 
