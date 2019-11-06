147017ee00c6317ab949aefd5332237d990f1f4d
sichoi <clogic@toss.im>
2019-11-06 22:15:40 +0900

    아아아악어떻게하냐공ㅇㅇㅇ

--- src/pages/CreateChatComplete/index.tsx
+++ src/pages/CreateChatComplete/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Button, ShareLink} from 'components';
+import { Button, ShareLink } from 'components';
 import * as styled from "./style";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { RootState } from 'store/reducers/interface';
 import { useSelector } from 'react-redux';
 
