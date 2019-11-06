544d9c2ecee53e10a2dd7c75278b2cdd9cca2167
권재원 <springkjw@gmail.com>
2019-11-06 22:37:24 +0900

    채팅방 완성 스타일 수정

--- src/components/ShareLink/style.ts
+++ src/components/ShareLink/style.ts
@@ -3,14 +3,33 @@ import { Props, StyleProps } from './interface';
 
 export const LinkBox = styled.div`
   font-size: 18px;
-  height: 35px;
-  margin: auto;
-  padding-top: 7px;
-  border: solid 0.5px #9b9b9b;
-  width: 470px;
+  height: 48px;
+  border: solid 1px #dedede;
+  background-color: #f9f9f9;
+  width: 580px;
   cursor: pointer;
+  position: relative;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 50px;
+
+
+  span {
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    color: #2b2b2b;
+  }
 `;
 
 export const Link = styled.img`
   margin-right: 8px;
+  position: absolute;
+  left: 16px;
+  top: 20px;
 `;
